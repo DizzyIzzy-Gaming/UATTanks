1caa31938e95975db16f2b2632fdaacd4e383e14
Isaiah <golfwang8363@gmail.com>
2020-12-22 03:58:52 -0700

    initial stuffs

--- Assets/Scripts/TankData.cs
+++ Assets/Scripts/TankData.cs
@@ -2,8 +2,17 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(TankMover))]
+
 public class TankData : MonoBehaviour
 {
+
     public float moveSpeed;
     public float rotateSpeed;
+    public TankMover mover;
+
+	void Start()
+	{
+		mover = GetComponent<TankMover>();
+	}
 }

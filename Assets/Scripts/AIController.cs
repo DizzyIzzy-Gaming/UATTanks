6e5520658a8b4793f777dceafca4a0835417cf00
Isaiah Sanchez <isasanch@uat.edu>
2021-04-30 01:17:22 -0700

    added a little AI

--- Assets/Scripts/AIController.cs
+++ Assets/Scripts/AIController.cs
@@ -2,29 +2,131 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+
+[RequireComponent(typeof(TankData))]
+[RequireComponent(typeof(TankMotor))]
+[RequireComponent(typeof(TankShooter))]
 public class AIController : MonoBehaviour
 {
 
-    public TankData td;
-    public List<Waypoint> waypoints;
-    public int currentWaypointIndex = 0;
+    public TankData tData;
+    public TankMotor tMotor;
+    public TankShooter tShooter;
+    public GameObject[] waypoints;
+    public int currentWaypoint = 0;
+    public float closeEnough = 1.0f;
+
+    public enum LoopType{ Stop, Loop, PingPong};
+    public LoopType loopType = LoopType.Stop;
+
+    private bool isLoopingForward = true;
     // Start is called before the first frame update
     void Start()
     {
-        
+        tData = GetComponent<TankData>();
+        tMotor = GetComponent<TankMotor>();
+        tShooter = GetComponent<TankShooter>();
     }
 
     // Update is called once per frame
     void Update()
     {
+
+
+
+        //turn towards waypoint
+        if(tMotor.RotateTowards(waypoints[currentWaypoint].transform.position, tData.rotateSpeed))
+		{
+            //do nothing
+		}
+		else
+		{
+            //move forward
+            tMotor.Move(tData.moveSpeed);
+
+		}
+
+
+        if(loopType == LoopType.Stop)
+		{
+            if (Vector3.SqrMagnitude(transform.position - waypoints[currentWaypoint].transform.position) < (closeEnough * closeEnough))
+            {
+                if (currentWaypoint < (waypoints.Length - 1))
+                {
+                    //Advance to the next waypoint
+                    currentWaypoint++;
+                }
+            }
+        }
+        else if (loopType == LoopType.Loop)
+		{
+            
+            
+            if (Vector3.SqrMagnitude(transform.position - waypoints[currentWaypoint].transform.position) < (closeEnough * closeEnough))
+            {
+                if(currentWaypoint < (waypoints.Length-1))
+				{
+                    //Advance to the next waypoint
+                    currentWaypoint++;
+                }
+				else
+				{
+                    currentWaypoint = 0;
+				}
+            }
+            
+			
+        }
+        else if (loopType == LoopType.PingPong)
+		{
+            if(isLoopingForward)
+			{
+                if (Vector3.SqrMagnitude(transform.position - waypoints[currentWaypoint].transform.position) < (closeEnough * closeEnough))
+                {
+                    if (currentWaypoint < (waypoints.Length - 1))
+                    {
+                        //Advance to the next waypoint
+                        currentWaypoint++;
+                    }
+                    else
+                    {
+                        isLoopingForward = false;
+                    }
+                }
+            }
+            else
+            {
+                if (Vector3.SqrMagnitude(transform.position - waypoints[currentWaypoint].transform.position) < (closeEnough * closeEnough))
+                {
+                    if (currentWaypoint > 0)
+                    {
+                        //Advance to the next waypoint
+                        currentWaypoint--;
+                    }
+                    else
+                    {
+                        isLoopingForward = true;
+                    }
+                }
+            }
+        }
+        else
+		{
+            Debug.LogWarning("[AIController] Unexpected LoopType");
+		}
+        
+        
+        
+        //move forward
+        //if we are "close enough" to the waypoint, advance to next waypoint
+
+
         DoPatrol();
     }
 
     public void DoPatrol ()
 	{
-        //turn towards waypoint
-        //move forward
-        //if we are "close enough" to the waypoint, advance to next waypoint
+        
 
 	}
 }
--- Assets/Scripts/TankMotor.cs
+++ Assets/Scripts/TankMotor.cs
@@ -28,9 +28,11 @@ public class TankMotor : MonoBehaviour
 
     public void Move(float speed)
     {
+        //Vector that holds speed data
         Vector3 speedVector = tf.forward * speed;
 
         characterController.SimpleMove(speedVector);
+        //transform.SetPositionAndRotation(transform.position, Quaternion.Euler(0, transform.rotation.y, 0));
     }
 
     public void Rotate(float speed)
@@ -39,4 +41,22 @@ public class TankMotor : MonoBehaviour
 
         tf.Rotate(rotateVector, Space.Self);
     }
+
+    public bool RotateTowards(Vector3 target, float rotateSpeed)
+	{
+        Vector3 adjustedTarget = new Vector3(target.x, transform.position.y, target.z);
+        Vector3 vectorToTarget = adjustedTarget - transform.position;
+        //Vector3 vectorToTarget = target - transform.position;
+        Quaternion targetRotation = Quaternion.LookRotation(vectorToTarget);
+
+        if (targetRotation == transform.rotation)
+		{
+            return false;
+		}
+
+        transform.rotation = Quaternion.RotateTowards(transform.rotation, targetRotation, rotateSpeed * Time.deltaTime);
+
+        // TODO: finish this method
+        return false;
+	}
 }
